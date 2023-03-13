2f31715b3ac80cdb62b83ec8b7dcf106206fdf22
Staslios <stanislav.stasyuk@eurobet.it>
2023-03-13 13:48:58 +0100

    deleted unused npm deps && fixed typos

--- src/js/components/Map/Map.jsx
+++ src/js/components/Map/Map.jsx
@@ -3,6 +3,12 @@ import { MapContainer, TileLayer } from 'react-leaflet'
 import MarkerList from "./MarkerList.jsx";
 
 
+/**
+ * Leaflet map standardized component
+ * @param markers {array} - contains markers to render on map
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const Map = ({ markers = []}) => {
 
   return <MapContainer center={[48.8665, 2.3280]} zoom={15} scrollWheelZoom={true}>
